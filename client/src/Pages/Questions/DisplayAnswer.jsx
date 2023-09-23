04e97229825ce7d9d89e9870d34958c72813bb28
aenache99 <alexenache71@gmail.com>
2023-09-23 22:34:03 +0300

    + refactoring DisplayAnswer.jsx

--- client/src/Pages/Questions/DisplayAnswer.jsx
+++ client/src/Pages/Questions/DisplayAnswer.jsx
@@ -7,6 +7,63 @@ import { deleteAnswer, voteAnswer } from "../../actions/question";
 import upvote from "../../assets/sort-up.svg";
 import downvote from "../../assets/sort-down.svg";
 
+const VotingButtons = ({ ans, handleVote }) => (
+    <div className="question-votes">
+        <img
+            src={upvote}
+            alt="Upvote"
+            width="18"
+            className="votes-icon"
+            onClick={() => handleVote(ans._id, "upVote")}
+        />
+        <p className="votes-count">{ans.upVote.length - ans.downVote.length}</p>
+        <img
+            src={downvote}
+            alt="Downvote"
+            width="18"
+            className="votes-icon"
+            onClick={() => handleVote(ans._id, "downVote")}
+        />
+    </div>
+);
+
+const ActionButtons = ({ ans, handleShare, handleDelete, User }) => (
+    <div>
+        <button type="button" onClick={handleShare}>
+            Share
+        </button>
+        {User?.result?._id === ans?.userId && (
+            <button
+                type="button"
+                onClick={() => handleDelete(ans._id, ans.noOfAnswers)}
+            >
+                Delete
+            </button>
+        )}
+    </div>
+);
+
+const UserDetails = ({ ans }) => (
+    <div>
+        <p>answered {moment(ans.answeredOn).fromNow()}</p>
+        <Link
+            to={`/Users/${ans.userId}`}
+            className="user-link"
+            style={{ color: "#0086d8" }}
+        >
+            <Avatar
+                backgroundColor="lightgreen"
+                px="8px"
+                py="5px"
+                borderRadius="4px"
+            >
+                {ans.userAnswered.charAt(0).toUpperCase()}
+            </Avatar>
+            <div>{ans.userAnswered}</div>
+        </Link>
+    </div>
+);
+
 const Answer = ({
                     ans,
                     handleShare,
@@ -16,57 +73,12 @@ const Answer = ({
                 }) => (
     <div className="display-ans" key={ans._id}>
         <div className="question-details-container-2">
-            <div className="question-votes">
-                <img
-                    src={upvote}
-                    alt="Upvote"
-                    width="18"
-                    className="votes-icon"
-                    onClick={() => handleVote(ans._id, "upVote")}
-                />
-                <p className="votes-count">{ans.upVote.length - ans.downVote.length}</p>
-                <img
-                    src={downvote}
-                    alt="Downvote"
-                    width="18"
-                    className="votes-icon"
-                    onClick={() => handleVote(ans._id, "downVote")}
-                />
-            </div>
+            <VotingButtons ans={ans} handleVote={handleVote} />
             <div style={{ width: "100%" }}>
                 <p className="answer-body">{ans.answerBody}</p>
                 <div className="question-actions-user">
-                    <div>
-                        <button type="button" onClick={handleShare}>
-                            Share
-                        </button>
-                        {User?.result?._id === ans?.userId && (
-                            <button
-                                type="button"
-                                onClick={() => handleDelete(ans._id, ans.noOfAnswers)}
-                            >
-                                Delete
-                            </button>
-                        )}
-                    </div>
-                    <div>
-                        <p>answered {moment(ans.answeredOn).fromNow()}</p>
-                        <Link
-                            to={`/Users/${ans.userId}`}
-                            className="user-link"
-                            style={{ color: "#0086d8" }}
-                        >
-                            <Avatar
-                                backgroundColor="lightgreen"
-                                px="8px"
-                                py="5px"
-                                borderRadius="4px"
-                            >
-                                {ans.userAnswered.charAt(0).toUpperCase()}
-                            </Avatar>
-                            <div>{ans.userAnswered}</div>
-                        </Link>
-                    </div>
+                    <ActionButtons ans={ans} handleShare={handleShare} handleDelete={handleDelete} User={User} />
+                    <UserDetails ans={ans} />
                 </div>
             </div>
         </div>
@@ -85,7 +97,6 @@ const DisplayAnswer = ({ question, handleShare }) => {
     const handleVote = (answerId, value) => {
         if (!User) {
             alert(`You need to login to ${value === "upVote" ? "upvote" : "downvote"} an answer.`);
-            // Navigate users to login if they aren't authenticated.
         } else {
             dispatch(voteAnswer(id, answerId, value));
         }
