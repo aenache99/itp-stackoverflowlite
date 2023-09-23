9e8780502220b9fe9b82f7e00170dcb71a097907
aenache99 <alexenache71@gmail.com>
2023-09-23 21:56:08 +0300

    + refactoring AskQuestion.jsx

--- client/src/Pages/AskQuestion/AskQuestion.jsx
+++ client/src/Pages/AskQuestion/AskQuestion.jsx
@@ -16,21 +16,23 @@ const AskQuestion = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (User) {
-            if (questionTitle && questionBody && questionTags) {
-                dispatch(
-                    askQuestion(
-                        {
-                            questionTitle,
-                            questionBody,
-                            questionTags,
-                            userPosted: User.result.name,
-                        },
-                        navigate
-                    )
-                );
-            } else alert("Please enter all the fields.");
-        } else alert("You need to login to ask a question.");
+        if (!User) {
+            alert("You need to login to ask a question.");
+            return;
+        }
+
+        if (questionTitle && questionBody && questionTags) {
+            dispatch(
+                askQuestion({
+                    questionTitle,
+                    questionBody,
+                    questionTags: questionTags.split(" "),
+                    userPosted: User.result.name,
+                }, navigate)
+            );
+        } else {
+            alert("Please enter all the fields.");
+        }
     };
 
     const handleEnter = (e) => {
@@ -38,6 +40,7 @@ const AskQuestion = () => {
             setQuestionBody(questionBody + "\n");
         }
     };
+
     return (
         <div className="ask-question">
             <div className="ask-ques-container">
@@ -46,31 +49,21 @@ const AskQuestion = () => {
                     <div className="ask-form-container">
                         <label htmlFor="ask-ques-title">
                             <h4>Title</h4>
-                            <p>
-                                Be specific and imagine you’re asking a question to another
-                                person:
-                            </p>
+                            <p>Be specific and imagine you’re asking a question to another person:</p>
                             <input
                                 type="text"
                                 id="ask-ques-title"
-                                onChange={(e) => {
-                                    setQuestionTitle(e.target.value);
-                                }}
+                                onChange={(e) => setQuestionTitle(e.target.value)}
                                 placeholder="e.g. How to count the number of objects in a JavaScript array?"
                             />
                         </label>
                         <label htmlFor="ask-ques-body">
                             <h4>Body</h4>
-                            <p>
-                                Include all the information someone would need to answer your
-                                question:
-                            </p>
+                            <p>Include all the information someone would need to answer your question:</p>
                             <textarea
                                 name=""
                                 id="ask-ques-body"
-                                onChange={(e) => {
-                                    setQuestionBody(e.target.value);
-                                }}
+                                onChange={(e) => setQuestionBody(e.target.value)}
                                 cols="30"
                                 rows="10"
                                 onKeyPress={handleEnter}
@@ -82,18 +75,12 @@ const AskQuestion = () => {
                             <input
                                 type="text"
                                 id="ask-ques-tags"
-                                onChange={(e) => {
-                                    setQuestionTags(e.target.value.split(" "));
-                                }}
-                                placeholder="e.g. (xml typescript wordpress)"
+                                onChange={(e) => setQuestionTags(e.target.value)}
+                                placeholder="e.g. xml typescript wordpress"
                             />
                         </label>
                     </div>
-                    <input
-                        type="submit"
-                        value="Post Question"
-                        className="review-btn"
-                    />
+                    <input type="submit" value="Post Question" className="review-btn" />
                 </form>
             </div>
         </div>
