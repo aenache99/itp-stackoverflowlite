af1e41dae1a708fa44039670b65dbc67bed0d72d
aenache99 <alexenache71@gmail.com>
2023-09-23 22:31:10 +0300

    + refactoring Auth.jsx

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -6,6 +6,7 @@ import "./Auth.css";
 import icon from "../../assets/icon.png";
 import AboutAuth from "./AboutAuth";
 import { signup, login } from "../../actions/auth";
+
 const Auth = () => {
     const [isSignup, setIsSignup] = useState(false);
     const [name, setName] = useState("");
@@ -22,19 +23,23 @@ const Auth = () => {
         setPassword("");
     };
 
+    const isFormValid = () => {
+        if (!email || !password) {
+            alert("Enter email and password");
+            return false;
+        }
+        if (isSignup && !name) {
+            alert("Enter a name to continue");
+            return false;
+        }
+        return true;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (!email && !password) {
-            alert("Enter email and password");
-        }
-        if (isSignup) {
-            if (!name) {
-                alert("Enter a name to continue");
-            }
-            dispatch(signup({ name, email, password }, navigate));
-        } else {
-            dispatch(login({ email, password }, navigate));
-        }
+        if (!isFormValid()) return;
+        const action = isSignup ? signup : login;
+        dispatch(action({ name, email, password }, navigate));
     };
 
     return (
@@ -43,51 +48,9 @@ const Auth = () => {
             <div className="auth-container-2">
                 <img src={icon} alt="stack overflow" className="login-logo" />
                 <form onSubmit={handleSubmit}>
-                    {isSignup && (
-                        <label htmlFor="name">
-                            <h4>Display Name</h4>
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                value={name}
-                                onChange={(e) => {
-                                    setName(e.target.value);
-                                }}
-                            />
-                        </label>
-                    )}
-                    <label htmlFor="email">
-                        <h4>Email</h4>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => {
-                                setEmail(e.target.value);
-                            }}
-                        />
-                    </label>
-                    <label htmlFor="password">
-                        <div style={{ display: "flex", justifyContent: "space-between" }}>
-                            <h4>Password</h4>
-                            {!isSignup && (
-                                <p style={{ color: "#007ac6", fontSize: "13px" }}>
-                                    forgot password?
-                                </p>
-                            )}
-                        </div>
-                        <input
-                            type="password"
-                            name="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => {
-                                setPassword(e.target.value);
-                            }}
-                        />
-                    </label>
+                    {isSignup && <TextInput label="Display Name" value={name} setValue={setName} />}
+                    <TextInput label="Email" value={email} setValue={setEmail} />
+                    <PasswordInput isSignup={isSignup} value={password} setValue={setPassword} />
                     <button type="submit" className="auth-btn">
                         {isSignup ? "Sign up" : "Log in"}
                     </button>
@@ -106,5 +69,36 @@ const Auth = () => {
         </section>
     );
 };
+
+const TextInput = ({ label, value, setValue }) => (
+    <label htmlFor={label.toLowerCase()}>
+        <h4>{label}</h4>
+        <input
+            type="text"
+            id={label.toLowerCase()}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+    </label>
+);
+
+const PasswordInput = ({ isSignup, value, setValue }) => (
+    <label htmlFor="password">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <h4>Password</h4>
+            {!isSignup && (
+                <p style={{ color: "#007ac6", fontSize: "13px" }}>
+                    forgot password?
+                </p>
+            )}
+        </div>
+        <input
+            type="password"
+            id="password"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+        />
+    </label>
+);
 
 export default Auth;
